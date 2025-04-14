fee551e4c095669f8153ec2c6172d1176fab3a58
Fernando Alvarado <fernandoalvarado@ciencias.unam.mx>
2025-04-13 20:41:03 -0600

    Agregando primer proyecto python

---
+++ Proyectos/Basico/numero.py
@@ -0,0 +1,71 @@
+# Proyecto realizado por: [Fernando Alvarado Palacios]
+# Fecha: [2025-4-13]
+#Descriccion del programa:
+# En este proyecto se tratar de adivinar numeros, el usuario que se hacerque mas gana
+
+import random 
+
+
+#Definiendo el constructor del juego
+class Juego:
+    def __init__(self):
+        self.intentos = 0
+        self.numero_secreto = random.randint(1, 100)
+        self.max= 100
+        self.min= 1
+        
+    def jugador(self, numero):
+        if(numero == self.numero_secreto):
+            print("Ganaste")
+            return 0
+        else:
+            if numero > self.numero_secreto:
+                print("El numero es menor")
+            else:
+                print("El numero es mayor")
+            return 1
+                   
+    def compu(self):
+        numero = random.randint(self.min, self.max)
+        if(numero == self.numero_secreto):
+            print("La compu gano")
+            return 0
+        else:
+            if numero > self.numero_secreto:
+                self.max = numero
+            else:
+                self.min = numero
+            return 1
+    
+        
+        
+        
+
+if __name__ == "__main__":
+    
+    print("En este juego, jugaras un juego de adiviar un numero del 1 al 100 contra la compu, cada uno tendra un turno y quien adivine primero gana")
+    juego = Juego()
+    salida = 1
+    while(salida != 0):
+        valor = input("Dame un numero entre 1 y 100: ")
+        try:
+            valor = int(valor)
+            salida = juego.jugador(valor)
+            if salida != 0:
+                print("La compu esta pensando")
+                salida = juego.compu()
+            
+        except ValueError:
+            print("No es un numero")
+        
+    
+    
+    
+    
+    
+    
+
+
+
+        
+        
